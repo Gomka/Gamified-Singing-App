91179300baeb50864a3e7eaa7f66234f04e67b55
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2025-02-03 19:33:42 +0100

    Created scriptable objects

---
+++ Gamified Singing App/Assets/Scripts/Glass.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Glass", menuName = "Scriptable Objects/Glass")]
+public class Glass : ScriptableObject
+{
+    public float frequencyBreak, toughness;
+    public AudioClip sound;
+    public Sprite sprite;
+    
+}
