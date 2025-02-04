be090dde8804200498bd50a3b55ae958a07745b3
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2025-02-04 18:18:06 +0100

    nicer ScriptableObject UI editing

--- Gamified Singing App/Assets/Scripts/Glass.cs
+++ Gamified Singing App/Assets/Scripts/Glass.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Glass", menuName = "Scriptable Objects/Glass")]
 public class Glass : ScriptableObject
 {
-    public float frequencyBreak, toughness;
+    [Range(40, 1200)]
+    public float frequencyBreak;
+    [Range(0.5f, 3)]
+    public float toughness;
     public AudioClip sound;
     public Sprite sprite;
 }
