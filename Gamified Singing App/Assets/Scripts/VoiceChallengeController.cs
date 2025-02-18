959a16f6ca36847b740f9d8affaab15c16e72691
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2025-02-18 16:51:52 +0100

    Progress with UI elements

--- Gamified Singing App/Assets/Scripts/VoiceChallengeController.cs
+++ Gamified Singing App/Assets/Scripts/VoiceChallengeController.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class VoiceChallengeController : MonoBehaviour
 {
     [SerializeField] private Exercise exercise;
-    [SerializeField] private Transform spawnPosition, endPosition;
+    [SerializeField] private Transform spawnPosition, middlePosition, endPosition;
     private Glass currentGlass;
     public float movementDuration = 20.0f;
+    //[SerializeField] GameObject prefabGlass;
+    //[SerializeField] RectTransform parentPanel;
 
     public void Start()
     {
@@ -15,7 +18,7 @@ public class VoiceChallengeController : MonoBehaviour
 
         // Feed the 1st glass to the player so they can sing
         exercise.Reset();
-        //NextGlass();
+        Invoke("NextGlass", 1.0f); 
     }
 
     public void NextGlass()
@@ -23,7 +26,7 @@ public class VoiceChallengeController : MonoBehaviour
         currentGlass = exercise.NextGlass();
 
         // If no more Glasses, win!
-        if (currentGlass == null)
+        if (currentGlass == null)  // AND all present glasses are won
         {
             Debug.Log("Sacabao");
             // WIN ~ SceneManager.LoadScene(0);
@@ -32,24 +35,34 @@ public class VoiceChallengeController : MonoBehaviour
 
         // Make glass appear 
         // TODO prettier animation
-        GameObject newGlass = new GameObject();
 
+
+        GameObject newGlass = new GameObject();
         newGlass.AddComponent<SpriteRenderer>().sprite = currentGlass.sprite;
 
         // Move the glass
         newGlass.transform.position = spawnPosition.position;
-        newGlass.transform.DOMove(endPosition.position, movementDuration).SetEase(Ease.Linear).OnComplete(() => GlassEnd(newGlass));
+        
+        //newGlass.transform.DOMove(endPosition.position, movementDuration).SetEase(Ease.Linear).OnComplete(() => GlassEnd(newGlass)); // Go to end
+        newGlass.transform.DOMove(middlePosition.position, movementDuration / 5); // Go to middle
 
-        // TODO make linear glass movement (?)
         // TODO end animation
-        // TODO scale glass according to pitch, Ideally scaling should be minimal, and sprite should be sized according to pitch
+        // TODO scale glass according to pitch, Ideally scaling should be minimal, and sprite should be sized according to pitch. Unlikely due to aspect ratios.
         // TODO BIG ONE: make pitch interact with glass
         // Y mil cosas mas que saldrán, pero eso pa luego
+
+        //newGlass.AddComponent<Button>().onClick.AddListener(() => GlassClicked(currentGlass.sound));
     }
 
     public void GlassEnd(GameObject go)
     {
-        Debug.Log("reached the end");
         GameObject.Destroy(go);
+        NextGlass();
+    }
+
+    public void GlassClicked(AudioClip glassSound)
+    {
+        Debug.Log("Ding!");
+        //Play(glassSound);
     }
 }
