bdb37a0f6ee2dccd9400704261221adf40a9c749
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2025-02-22 14:12:26 +0100

    converting to my own visualization system

--- Gamified Singing App/Assets/Scripts/AudioRecorder.cs
+++ Gamified Singing App/Assets/Scripts/AudioRecorder.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioRecorder : MonoBehaviour
 {
     public AudioSource audioSource;
     public int duration = 8;
+    public AudioMixerGroup microphoneMixer;
 
     void Start()
     {
+        audioSource.outputAudioMixerGroup = microphoneMixer; // Set to a different channel so no echo is heard
         audioSource.clip = Microphone.Start(string.Empty, audioSource.loop, duration, AudioSettings.outputSampleRate);
         audioSource.Play();
     }
--- Gamified Singing App/Assets/Scripts/VoiceChallengeController.cs
+++ Gamified Singing App/Assets/Scripts/VoiceChallengeController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.Audio;
+using UnityEngine.Timeline;
 
 public class VoiceChallengeController : MonoBehaviour
 {
@@ -11,6 +13,10 @@ public class VoiceChallengeController : MonoBehaviour
     //[SerializeField] GameObject prefabGlass;
     //[SerializeField] RectTransform parentPanel;
 
+    public AudioPitchEstimator estimator;
+
+    public int estimateRate = 30;
+
     public void Start()
     {
         // Start the scene
@@ -18,7 +24,10 @@ public class VoiceChallengeController : MonoBehaviour
 
         // Feed the 1st glass to the player so they can sing
         exercise.Reset();
-        Invoke("NextGlass", 1.0f); 
+        Invoke("NextGlass", 1.0f);
+
+        // call at slow intervals (Update() is generally too fast)
+        InvokeRepeating(nameof(UpdateVisualizer), 0, 1.0f / estimateRate);
     }
 
     public void NextGlass()
@@ -63,5 +72,61 @@ public class VoiceChallengeController : MonoBehaviour
     {
         Debug.Log("Ding!");
         //Play(glassSound);
+    }
+
+    void UpdateVisualizer()
+    {
+        // estimate the fundamental frequency
+        var frequency = estimator.Estimate(audioSource);
+
+        // visualize SRH score
+        var srh = estimator.SRH;
+        var numPoints = srh.Count;
+        var positions = new Vector3[numPoints];
+        for (int i = 0; i < numPoints; i++)
+        {
+            var position = (float)i / numPoints - 0.5f;
+            var value = srh[i] * 0.005f;
+            positions[i].Set(position, value, 0);
+        }
+        lineSRH.positionCount = numPoints;
+        lineSRH.SetPositions(positions);
+
+        // visualize fundamental frequency
+        if (float.IsNaN(frequency))
+        {
+            // hide the line when it does not exist
+            lineFrequency.positionCount = 0;
+        }
+        else
+        {
+            var min = estimator.frequencyMin;
+            var max = estimator.frequencyMax;
+            var position = (frequency - min) / (max - min) - 0.5f;
+
+            // indicate the frequency with LineRenderer
+            lineFrequency.positionCount = 2;
+            lineFrequency.SetPosition(0, new Vector3(position, +1, 0));
+            lineFrequency.SetPosition(1, new Vector3(position, -1, 0));
+
+            // indicate the latest frequency with TextMesh
+            marker.position = new Vector3(position, 0, 0);
+            textFrequency.text = string.Format("{0}\n{1:0.0} Hz", GetNameFromFrequency(frequency), frequency);
+        }
+
+        // visualize lowest/highest frequency
+        textMin.text = string.Format("{0} Hz", estimator.frequencyMin);
+        textMax.text = string.Format("{0} Hz", estimator.frequencyMax);
+    }
+
+
+    // frequency -> pitch name
+    string GetNameFromFrequency(float frequency)
+    {
+        var noteNumber = Mathf.RoundToInt(12 * Mathf.Log(frequency / 440) / Mathf.Log(2) + 69);
+        string[] names = {
+            "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+        };
+        return names[noteNumber % 12];
     }
 }
